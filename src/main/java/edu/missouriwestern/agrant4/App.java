7a3a8147c636bffabab48dbd78a863bd14e6a7cd
abg5043 <aaron.b.grant@gmail.com>
2022-03-14 21:26:54 -0500

    Added openCSV dependency and finalized Client object.

--- src/main/java/edu/missouriwestern/agrant4/App.java
+++ src/main/java/edu/missouriwestern/agrant4/App.java
@@ -14,28 +14,34 @@ public class App
     //Create logger instance
     public final static Logger LOG = LogManager.getLogger(App.class);
 
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
         LOG.info("Program started");
         LOG.trace("Inside main function");
 
-        String action = args[0];
-        String reportName = args[1];
-        LOG.info("Step 1: Type of action: " + action);
-        LOG.info("Step 1: Name of report: " + reportName);
+        try {
+            String action = args[0];
+            String reportName = args[1];
+            LOG.info("Step 1: Type of action: " + action);
+            LOG.info("Step 1: Name of report: " + reportName);
 
 
-        if(action.equals("query")) {
-            //this is a query
-            String clientFile = args[2];
-            String clientNumber = args[3];
-            String currency = args[4];
-            LOG.info("Step 1: Client file: " + clientFile);
-            LOG.info("Step 1: Client number: " + clientNumber);
-            LOG.info("Step 1: Currency type: " + currency);
+            if(action.equals("query")) {
+                //this is a query
+                String clientFile = args[2];
+                String clientNumber = args[3];
+                String currency = args[4];
+                LOG.info("Step 1: Client file: " + clientFile);
+                LOG.info("Step 1: Client number: " + clientNumber);
+                LOG.info("Step 1: Currency type: " + currency);
 
-        } else {
-            //this is a report
+            } else {
+                //this is a report
+                //TODO: Finish this
+            }
+        } catch (Exception e) {
+            LOG.error("Error reading from command line: " + e.getMessage());
+            System.err.println("Error reading url from command line: " + e.getMessage());
+            System.exit(1);
         }
 
 
--- src/main/java/edu/missouriwestern/agrant4/Clients.java
+++ src/main/java/edu/missouriwestern/agrant4/Clients.java
@@ -17,25 +17,156 @@ public class Clients {
   private String website;
 
 
-
   public Clients() {
-
+    //TODO: fix this
   }
 
   public Clients(int clientNumber, String firstName, String lastName, String company, String address, String city, String country, String state, int postalCode, double riskFactor, String phone1, String phone2, String email, String website) {
+    setClientNumber(clientNumber);
+    setFirstName(firstName);
+    setLastName(lastName);
+    setCompany(company);
+    setAddress(address);
+    setCity(city);
+    setCountry(country);
+    setState(state);
+    setPostalCode(postalCode);
+    setRiskFactor(riskFactor);
+    setPhone1(phone1);
+    setPhone2(phone2);
+    setEmail(email);
+    setWebsite(website);
+  }
+
+  public int getClientNumber() {
+    return clientNumber;
+  }
+
+  public void setClientNumber(int clientNumber) {
     this.clientNumber = clientNumber;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
     this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
     this.lastName = lastName;
+  }
+
+  public String getCompany() {
+    return company;
+  }
+
+  public void setCompany(String company) {
     this.company = company;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public void setAddress(String address) {
     this.address = address;
+  }
+
+  public String getCity() {
+    return city;
+  }
+
+  public void setCity(String city) {
     this.city = city;
+  }
+
+  public String getCountry() {
+    return country;
+  }
+
+  public void setCountry(String country) {
     this.country = country;
+  }
+
+  public String getState() {
+    return state;
+  }
+
+  public void setState(String state) {
     this.state = state;
+  }
+
+  public int getPostalCode() {
+    return postalCode;
+  }
+
+  public void setPostalCode(int postalCode) {
     this.postalCode = postalCode;
+  }
+
+  public double getRiskFactor() {
+    return riskFactor;
+  }
+
+  public void setRiskFactor(double riskFactor) {
     this.riskFactor = riskFactor;
+  }
+
+  public String getPhone1() {
+    return phone1;
+  }
+
+  public void setPhone1(String phone1) {
     this.phone1 = phone1;
+  }
+
+  public String getPhone2() {
+    return phone2;
+  }
+
+  public void setPhone2(String phone2) {
     this.phone2 = phone2;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
     this.email = email;
+  }
+
+  public String getWebsite() {
+    return website;
+  }
+
+  public void setWebsite(String website) {
     this.website = website;
+  }
+
+  @Override
+  public String toString() {
+    return "Clients{" +
+        "clientNumber=" + clientNumber +
+        ", firstName='" + firstName + '\'' +
+        ", lastName='" + lastName + '\'' +
+        ", company='" + company + '\'' +
+        ", address='" + address + '\'' +
+        ", city='" + city + '\'' +
+        ", country='" + country + '\'' +
+        ", state='" + state + '\'' +
+        ", postalCode=" + postalCode +
+        ", riskFactor=" + riskFactor +
+        ", phone1='" + phone1 + '\'' +
+        ", phone2='" + phone2 + '\'' +
+        ", email='" + email + '\'' +
+        ", website='" + website + '\'' +
+        '}';
   }
 }
